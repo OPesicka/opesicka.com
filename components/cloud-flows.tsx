2b1082422cf0c41736edffcab74a0952337018af
OPesicka <ondrejpesicka@gmail.com>
2023-12-26 11:03:58 +0100

    bump flows

--- components/cloud-flows.tsx
+++ components/cloud-flows.tsx
@@ -1,17 +1,30 @@
 'use client'
 
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 
 import '@rbnd/flows/flows.css'
 import { init } from '@rbnd/flows/cloud'
 
 export const CloudFlows = () => {
+  const firstRender = useFirstRender()
+
   useEffect(() => {
+    if (firstRender) return
     void init({
       customApiUrl: 'https://api.stage.flows-cloud.com',
-      projectId: 'OPesicka-web'
+      projectId: '0dc4b250-9a79-4a64-a50e-9fb90082e00d'
     })
-  }, [])
+  }, [firstRender])
 
   return null
+}
+
+export const useFirstRender = (): boolean => {
+  const [firstRender, setFirstRender] = useState(true)
+
+  useEffect(() => {
+    setFirstRender(false)
+  }, [])
+
+  return firstRender
 }
